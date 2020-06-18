709c701c159d2beb448e151f00e63aa5f3dfa281
Renan Borges <rbgmarketplay@gmail.com>
2020-06-18 20:27:21 -0300

    Milestone 2  Shrines
    Projeto Milestone 2 Shrines

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public string ScenePoint;
+    public Transform BackPoint;
+
+    public bool mustactivatereturn;
+   
+    public int ID;
+
+    static bool mustreturn;
+    static int returnID;
+
+    Color transparent = new Color(0, 0, 0, 0);
+
+
+    void Start() {
+
+        Time.timeScale = 1;
+        if (mustreturn && ID == returnID) {
+
+         GameObject playerGB = GameObject.FindWithTag("Player");
+         Transform playerTR = playerGB.GetComponent<Transform>();
+         playerTR.position = BackPoint.position;
+         playerTR.rotation = BackPoint.rotation;
+            Debug.Log("P: " + playerGB.name + " -> " + BackPoint.position.ToString(), BackPoint.gameObject);
+        }
+        
+    }
+
+
+    void OnTriggerEnter (Collider col)
+    {
+        if (col.tag == "Player") {
+            Time.timeScale = 0;
+            if (mustactivatereturn) { 
+                mustreturn = true;
+                }
+            
+            returnID = ID;
+            StartCoroutine(LoadingScene());
+           
+        }
+    }
+
+            IEnumerator LoadingScene()
+    {
+        Transicao transicao = FindObjectOfType<Transicao>();
+        transicao.StartTransicao(transparent, Color.black);
+        yield return new WaitUntil(() => transicao.finished);
+        SceneManager.LoadScene(ScenePoint);
+    }
+}
---
+++ Assets/Scripts/Transicao.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Transicao : MonoBehaviour
+{
+    public Image TransitionImage;
+    public float vel;
+    public bool finished;
+
+    Color transparent = new Color(0, 0, 0, 0);
+
+    void Start()
+    {
+        StartTransicao(Color.black, transparent);
+    }
+
+    public void StartTransicao(Color Start, Color End)
+    {
+        finished = false;
+        StartCoroutine(DoTransition(Start, End));
+    }
+
+
+    IEnumerator DoTransition(Color Start, Color End)
+    {
+        float t = 0;
+
+        while (true)
+        {
+            t = t + Time.unscaledDeltaTime * vel;
+            TransitionImage.color = Color.Lerp(Start, End, t);
+            if (t >= 1)
+            {
+                break;
+            }
+            yield return new WaitForEndOfFrame();
+        }
+
+        finished = true;
+    }
+}
